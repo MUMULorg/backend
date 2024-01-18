0dc44f190036764a63c3e2a61bfbf8641b9f94f4
silver0-stack <dev.choiey@gmail.com>
2024-01-18 23:27:23 +0900

    credential true setting

--- src/main/java/com/example/ama_backend/config/WebConfig.java
+++ src/main/java/com/example/ama_backend/config/WebConfig.java
@@ -16,18 +16,21 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://54.180.80.63:3000")
                 .allowedOrigins("http://54.180.80.63")
 
-                .allowedOrigins("http://13.124.15.169") // fix: "/" 끝에 지우기
+                .allowedOrigins("http://13.124.15.169/") // fix: "/" 끝에 지우기
 
                 .allowedOrigins("https://mumul.site")
                 .allowedOrigins("http://mumul.site")
 
-                .allowedOriginPatterns("*") // 500 internal server error 해결
+//                .allowedOriginPatterns("*")
 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false)
-                .exposedHeaders("Authorization")         // <----- 추가함 2023.06.06
-                .exposedHeaders("Access-Control-Allow-Origin"); // "Access-Control-Allow-Origin" 헤더를 노출합니다.
+                .allowCredentials(true) // false -> true : 쿠키 인증 요청 허용
+                .exposedHeaders("Authorization")
+                .exposedHeaders("Access-Control-Allow-Origin") // "Access-Control-Allow-Origin" 헤더를 노출합니다.
+                .maxAge(3600); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
+
     }
 
 }
