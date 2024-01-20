a00a49f8b68d931cb87efa3e197f4c2a320cbcef
silver0-stack <dev.choiey@gmail.com>
2024-01-20 12:23:34 +0900

    cors uri + /

--- src/main/java/com/example/ama_backend/config/WebConfig.java
+++ src/main/java/com/example/ama_backend/config/WebConfig.java
@@ -6,27 +6,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    // addCorsMappings : CORS 설정을 추가하는 메소드이다.
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("http://13.124.15.169")
+                .allowedOriginPatterns("http://13.124.15.169/")
+                .allowedOriginPatterns("http://54.180.80.63:3000")
+                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOriginPatterns("http://54.180.80.63")
+                .allowedOriginPatterns("https://mumul.site/")
+                .allowedOriginPatterns("http://mumul.site/")
 
-                .allowedOrigins("http://54.180.80.63:3000")
-                .allowedOrigins("http://54.180.80.63")
+//                .allowedOriginPatterns("*")   // .allowCredentails(true)랑 같이 못쓰는 와일드 카드임
 
-                .allowedOrigins("http://13.124.15.169/") // fix: "/" 끝에 지우기
-
-                .allowedOrigins("https://mumul.site")
-                .allowedOrigins("http://mumul.site")
-
-//                .allowedOriginPatterns("*")
-
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true) // false -> true : 쿠키 인증 요청 허용
-                .exposedHeaders("Authorization")
+                .exposedHeaders("Authorization","RefreshToken")
                 .exposedHeaders("Access-Control-Allow-Origin") // "Access-Control-Allow-Origin" 헤더를 노출합니다.
                 .maxAge(3600); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
 
